6f43ccf0a5d995308fb0eca60e1787252b14c0b6
Bartosz Stachowiak <sbartekt@op.pl>
2021-12-10 12:46:18 +0100

    Added random country and city yielding

--- src/bst/Main.java
+++ src/bst/Main.java
@@ -1,8 +1,15 @@
 package bst;
 
-public class Main {
+import simulation.random.RandomDataGenerator;
 
-    public static void main(String[] args) {
-	// write your code here
+import java.io.IOException;
+
+public class Main {
+    private static void test() throws IOException {
+        var rand = RandomDataGenerator.getInstance();
+    }
+
+    public static void main(String[] args) throws IOException {
+        var rand = RandomDataGenerator.getInstance();
     }
 }
