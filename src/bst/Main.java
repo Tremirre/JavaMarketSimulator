1cbc0e5b78066749e0b627da25e19e16aaf8f7e0
Bartosz Stachowiak <sbartekt@op.pl>
2021-12-10 12:57:59 +0100

    Revert "Added random country and city yielding"
    This reverts commit 6f43ccf0a5d995308fb0eca60e1787252b14c0b6.

--- src/bst/Main.java
+++ src/bst/Main.java
@@ -1,15 +1,8 @@
 package bst;
 
-import simulation.random.RandomDataGenerator;
-
-import java.io.IOException;
-
 public class Main {
-    private static void test() throws IOException {
-        var rand = RandomDataGenerator.getInstance();
-    }
 
-    public static void main(String[] args) throws IOException {
-        var rand = RandomDataGenerator.getInstance();
+    public static void main(String[] args) {
+	// write your code here
     }
 }
--- src/simulation/random/RandomDataGenerator.java
+++ src/simulation/random/RandomDataGenerator.java
@@ -1,46 +1,17 @@
 package simulation.random;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Random;
-
 public class RandomDataGenerator {
     private static RandomDataGenerator instance;
-    private final HashMap<String, String[]> addressData;
-    private final Random generator;
 
-    private RandomDataGenerator() throws IOException {
-        final String path = "resource\\address_data\\";
-        var data = new HashMap<String, String[]>();
-        String countries = Files.readString(Path.of(path + "countries.txt"));
-        String[] countriesList = countries.split(";");
-        for (String s : countriesList) {
-            String cities = Files.readString(Path.of(path + s.toLowerCase() + ".txt"));
-            String[] citiesList = cities.split(";");
-            data.put(s, citiesList);
-        }
-        this.addressData = data;
-        this.generator = new Random();
+    private RandomDataGenerator() {
+
     }
 
     public String yieldCountry() {
-        var keys = this.addressData.keySet();
-        String[] countries = keys.toArray(new String[0]);
-        int randomIdx = generator.nextInt(countries.length);
-        return countries[randomIdx];
+        return "";
     }
 
-    public String yieldCityForCountry(String country) {
-        if (!this.addressData.containsKey(country))
-            return null;
-        String[] cities = this.addressData.get(country);
-        int randomIdx = generator.nextInt(cities.length);
-        return cities[randomIdx];
-    }
-
-    public static RandomDataGenerator getInstance() throws IOException{
+    public static RandomDataGenerator getInstance() {
         if (instance == null) {
             instance = new RandomDataGenerator();
         }
