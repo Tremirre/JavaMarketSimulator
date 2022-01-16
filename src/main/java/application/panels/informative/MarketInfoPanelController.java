870c3e37e8fa922a30ccd584314d6eb89d1b7aa1
Bartosz Stachowiak <sbartekt@op.pl>
2022-01-16 11:52:24 +0100

    Added info panel display for listviews on info panels

--- src/main/java/application/panels/informative/MarketInfoPanelController.java
+++ src/main/java/application/panels/informative/MarketInfoPanelController.java
@@ -1,10 +1,13 @@
 package application.panels.informative;
 
+import application.driver.MainController;
 import application.panels.Refreshable;
 import application.util.DecimalDisplayFormat;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseButton;
+import simulation.market.CurrenciesMarket;
 import simulation.market.Market;
 import simulation.market.StockMarket;
 
@@ -72,5 +75,24 @@ public class MarketInfoPanelController extends InfoPanelController implements Re
     public void passID(String marketName) {
         this.market = this.simulation.getMarketByName(marketName);
         this.updateFields();
+    }
+
+    public void passMainControllerReference(MainController controller) {
+        super.passMainControllerReference(controller);
+        this.assetListView.setOnMouseClicked( me -> {
+            if (me.getButton() == MouseButton.PRIMARY) {
+                int index = this.assetListView.getSelectionModel().getSelectedIndex();
+                if (index >= 0) {
+                    var id = this.assetListView.getItems().get(index);
+                    if (this.market instanceof StockMarket) {
+                        mainController.onListViewClicked("stock_index_info_panel.fxml", "Stock Index Info Panel", id);
+                    } else if (this.market instanceof CurrenciesMarket) {
+                        mainController.onListViewClicked("currency_asset_info_panel.fxml", "Currency Info Panel", id);
+                    } else {
+                        mainController.onListViewClicked("commodity_asset_info_panel.fxml", "Commodity Info Panel", id);
+                    }
+                }
+            }
+        });
     }
 }
--- src/main/java/application/panels/informative/StockIndexInfoPanelController.java
+++ src/main/java/application/panels/informative/StockIndexInfoPanelController.java
@@ -1,10 +1,14 @@
 package application.panels.informative;
 
+import application.driver.MainController;
 import application.util.DecimalDisplayFormat;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseButton;
 import simulation.market.StockMarketIndex;
+
+import java.util.ArrayList;
 
 public class StockIndexInfoPanelController extends InfoPanelController {
 
@@ -13,6 +17,8 @@ public class StockIndexInfoPanelController extends InfoPanelController {
     private Label indexValueLabel;
     @FXML
     private ListView<String> companyListView;
+    private ArrayList<String> stockIDs = new ArrayList<>();
+
 
     @Override
     public void refresh() {
@@ -20,11 +26,13 @@ public class StockIndexInfoPanelController extends InfoPanelController {
     }
 
     protected void updateFields() {
+        this.stockIDs.clear();
         this.companyListView.getItems().clear();
         this.nameLabel.setText(this.idx.getName());
         this.indexValueLabel.setText(new DecimalDisplayFormat(3).format(this.idx.calculateIndexValue()));
         for (var company : this.idx.getCompanies()) {
             this.companyListView.getItems().add(company.getCompanyName());
+            this.stockIDs.add(String.valueOf(company.getAssociatedAsset()));
         }
     }
 
@@ -32,5 +40,19 @@ public class StockIndexInfoPanelController extends InfoPanelController {
     public void passID(String id) {
         this.idx = this.simulation.getEntitiesManager().getIndexByName(id);
         this.updateFields();
+    }
+
+    public void passMainControllerReference(MainController controller) {
+        super.passMainControllerReference(controller);
+        this.companyListView.setOnMouseClicked( me -> {
+            if (me.getButton() == MouseButton.PRIMARY) {
+                int index = this.companyListView.getSelectionModel().getSelectedIndex();
+                if (index >= 0) {
+                    var id = this.stockIDs.get(index);
+                    mainController.onListViewClicked("company_info_panel.fxml", "Company Info Panel", id);
+                }
+
+            }
+        });
     }
 }
