0f965e3a4dec38eb6f3903735f93fffe59f74677
Bartosz Stachowiak <sbartekt@op.pl>
2022-01-17 22:52:20 +0100

    Modified resource structure

--- src/main/java/simulation/util/ResourceHolder.java
+++ src/main/java/simulation/util/ResourceHolder.java
@@ -4,11 +4,9 @@ import simulation.util.records.CommodityRecord;
 import simulation.util.records.CurrencyRecord;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.nio.file.*;
 import java.util.*;
 
